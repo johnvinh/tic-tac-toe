696340e864a7a0de0be62d792bf65cdda728614c
John Vinh <john.michael.vinh2@gmail.com>
2023-01-01 23:18:09 -0500

    Create layout and scene

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.tictactoe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +12,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.show();*/
+        Button btn1 = new Button("Say, Hello World");
+        StackPane root = new StackPane();
+        root.getChildren().add(btn1);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("First JavaFX Application");
         stage.show();
     }
 
