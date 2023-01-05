62cef4e15291126e4ddfc05252169442d30c79d8
John Vinh <john.michael.vinh2@gmail.com>
2023-01-04 23:00:53 -0500

    Add 3x3 tic tac toe grid of buttons

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -1,9 +1,12 @@
 package com.example.tictactoe;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -18,9 +21,29 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();*/
         Button btn1 = new Button("Say, Hello World");
-        StackPane root = new StackPane();
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("hello world");
+            }
+        });
+        /*StackPane root = new StackPane();
         root.getChildren().add(btn1);
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 600, 400);*/
+        GridPane root = new GridPane();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                Button btn = new Button(" ");
+                btn.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        btn.setText("X");
+                    }
+                });
+                root.add(btn, i, j);
+            }
+        }
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.setTitle("First JavaFX Application");
         stage.show();
